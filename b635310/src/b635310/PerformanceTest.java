2ba15696c87d6bc246519795244b580ddba2d6b3
jangs <jangs@DESKTOP-R46BTDF>
2024-06-14 15:27:08 +0900

    오류2

--- b635310/src/b635310/PerformanceTest.java
+++ b635310/src/b635310/PerformanceTest.java
@@ -15,12 +15,16 @@ public class PerformanceTest {
         
         // 책들을 id를 기준으로 오름차순 정렬
 	    Arrays.sort(books, (b1, b2) -> Integer.compare(b1.getId(), b2.getId()));
+	    Book temp_book = books[1000000];
 	    
-        long startTime = System.nanoTime(); // 성능 테스트 시작 시간
-        BookSearch.search_bs(books, books[500000].getId()); // search_bs 함수 시행
-        long endTime = System.nanoTime(); // 성능 테스트 종료 시간
-
-        System.out.println("search_bs 수행 시간: " + (endTime - startTime) + " 나노초");
+	    if(temp_book.getId()>=0 && temp_book.getId()<=999999)
+	    {
+	    	long startTime = System.nanoTime(); // 성능 테스트 시작 시간
+	        BookSearch.search_bs(books, temp_book.getId()); // search_bs 함수 시행
+	        long endTime = System.nanoTime(); // 성능 테스트 종료 시간
+	    }
+        
+	    System.out.println("search_bs 수행 시간: " + (endTime - startTime) + " 나노초");
     }
 
 }
